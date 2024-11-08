d5a03f74cbc60605eaee72a1017d6ecb98516e0e
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-08 16:40:42 +0100

    Add ResourceNodeUtility class

---
+++ Source/ResourceRoulette/Private/ResourceNodeUtility.cpp
@@ -0,0 +1,208 @@
+﻿#include "ResourceNodeUtility.h"
+#include "Misc/Paths.h"
+#include "EngineUtils.h"
+#include "HAL/FileManager.h"
+#include "FGResourceNode.h"
+#include "HAL/IConsoleManager.h"
+#include "ResourceNodeAssets.h"
+
+// UE_DISABLE_OPTIMIZATION
+DEFINE_LOG_CATEGORY_STATIC(LogResourceRoulette, Log, All);
+
+static TAutoConsoleVariable<int32> CVarLogLevel(
+	TEXT("ResourceRoulette.LogLevel"), 1,
+	TEXT("Sets log verbosity level: 0 = Error, 1 = Warning, 2 = Debug"),
+	ECVF_Default
+);
+
+FResourceNodeUtilityLog FResourceNodeUtilityLog::Instance;
+FResourceNodeUtilityLog& FResourceNodeUtilityLog::Get()
+{
+	return Instance;
+}
+
+void FResourceNodeUtilityLog::InitializeLog()
+{
+	FScopeLock Lock(&LogFileMutex);
+	if (!LogFile)
+	{
+		FString LogFilePath = FPaths::Combine(
+			FPlatformMisc::GetEnvironmentVariable(TEXT("LOCALAPPDATA")),
+			TEXT("FactoryGame/Saved/Logs/ResourceRoulette.log")
+		);
+		LogFile = new FOutputDeviceFile(*LogFilePath, true);
+		LogFile->SetAutoEmitLineTerminator(true);
+		GLog->AddOutputDevice(this);
+		LogMessage("Resource Roulette Module Log Startup", ELogLevel::Debug);
+	}
+}
+
+
+void FResourceNodeUtilityLog::Serialize(const TCHAR* V, ELogVerbosity::Type Verbosity, const FName& Category)
+{
+	if (Category == TEXT("LogResourceRoulette"))
+	{
+		FString LogMessage(V);
+
+		int32 PrefixEndIndex = LogMessage.Find(TEXT("LogResourceRoulette: "));
+		if (PrefixEndIndex != INDEX_NONE)
+		{
+			LogMessage = LogMessage.Mid(PrefixEndIndex + 19);
+		}
+		if (LogFile)
+		{
+			LogFile->Serialize(*LogMessage, Verbosity, Category);
+		}
+	}
+}
+
+void FResourceNodeUtilityLog::ShutdownLog()
+{
+	FScopeLock Lock(&LogFileMutex);
+	if (LogFile)
+	{
+		LogMessage("Resource Roulette Module Log Shutdown", ELogLevel::Debug);
+		GLog->RemoveOutputDevice(this);
+		delete LogFile;
+		LogFile = nullptr;
+	}
+}
+
+void FResourceNodeUtilityLog::LogMessage(const FString& Message, ELogLevel Level)
+{
+	int32 CurrentLogLevel = CVarLogLevel.GetValueOnAnyThread();
+	if (static_cast<int32>(Level) >= CurrentLogLevel)
+	{
+		FScopeLock Lock(&LogFileMutex);
+		switch (Level)
+		{
+		case ELogLevel::Error:
+			UE_LOG(LogResourceRoulette, Error, TEXT("%s"), *Message);
+			break;
+		case ELogLevel::Warning:
+			UE_LOG(LogResourceRoulette, Warning, TEXT("%s"), *Message);
+			break;
+		case ELogLevel::Debug:
+		default:
+			UE_LOG(LogResourceRoulette, Log, TEXT("%s"), *Message);
+			break;
+		}
+	}
+}
+
+void UResourceNodeUtility::InitializeLoggingModule()
+{
+	FResourceNodeUtilityLog::Get().InitializeLog();
+	FResourceNodeUtilityLog::Get().LogMessage("Logging initialized", ELogLevel::Debug);
+}
+
+bool UResourceNodeUtility::TryGetOreTypeFromName(const FString& Name, EOreType& OreType)
+{
+	for (auto& OrePair : OreTypeNameList)
+	{
+		if (Name.Equals(OrePair.Key, ESearchCase::IgnoreCase))
+		{
+			OreType = OrePair.Value;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UResourceNodeUtility::TryGetNameFromOreType(const EOreType OreType, FString& Name)
+{
+	for (auto& OrePair : OreTypeNameList)
+	{
+		if (OreType == OrePair.Value)
+		{
+			Name = OrePair.Key;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UResourceNodeUtility::FindOreTypeFromPath(const FString& Path, EOreType& OutOreType)
+{
+	for (const auto& OrePair : OreTypeNameList)
+	{
+		if (Path.Contains(OrePair.Key, ESearchCase::IgnoreCase))
+		{
+			OutOreType = OrePair.Value;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UResourceNodeUtility::GetOreTypeFromResourceNode(const AFGResourceNode* ResourceNode, EOreType& OutOreType)
+{
+	if (!ResourceNode)
+	{
+		return false;
+	}
+
+	// Check texture path first
+	UTexture2D* CompasTexture = UFGResourceDescriptor::GetCompasTexture(ResourceNode->GetResourceClass());
+	if (CompasTexture && FindOreTypeFromPath(CompasTexture->GetPathName(), OutOreType))
+	{
+		return true;
+	}
+	return FindOreTypeFromPath(ResourceNode->GetPathName(), OutOreType);
+}
+
+void UResourceNodeUtility::LogAllResourceNodes(const UWorld* World)
+{
+	FResourceNodeUtilityLog::Get().LogMessage("=== Begin Logging All AFGResourceNodes in the World ===",ELogLevel::Debug);
+
+	for (TActorIterator<AFGResourceNode> It(World); It; ++It)
+	{
+		AFGResourceNode* ResourceNode = *It;
+
+		FString NodeName = ResourceNode->GetName();
+		FString ResourceClass = ResourceNode->GetResourceClass()
+									? ResourceNode->GetResourceClass()->GetName()
+									: TEXT("None");
+
+		FString ResourceForm;
+		switch (ResourceNode->GetResourceForm())
+		{
+		case EResourceForm::RF_SOLID: ResourceForm = TEXT("Solid");
+			break;
+		case EResourceForm::RF_LIQUID: ResourceForm = TEXT("Liquid");
+			break;
+		case EResourceForm::RF_GAS: ResourceForm = TEXT("Gas");
+			break;
+		case EResourceForm::RF_HEAT: ResourceForm = TEXT("Heat");
+			break;
+		default: ResourceForm = FString::Printf(
+				TEXT("Unknown (%d)"), static_cast<int32>(ResourceNode->GetResourceForm()));
+			break;
+		}
+
+		FString ResourceAmount;
+		switch (ResourceNode->GetResourceAmount())
+		{
+		case EResourceAmount::RA_Poor: ResourceAmount = TEXT("Poor");
+			break;
+		case EResourceAmount::RA_Normal: ResourceAmount = TEXT("Normal");
+			break;
+		case EResourceAmount::RA_Rich: ResourceAmount = TEXT("Rich");
+			break;
+		case EResourceAmount::RA_Infinite: ResourceAmount = TEXT("Infinite");
+			break;
+		default: ResourceAmount = FString::Printf(
+				TEXT("Unknown (%d)"), static_cast<int32>(ResourceNode->GetResourceAmount()));
+			break;
+		}
+
+		FResourceNodeUtilityLog::Get().LogMessage(FString::Printf(
+			TEXT("%s|%s|%s|%s"),
+			*NodeName, *ResourceClass, *ResourceForm, *ResourceAmount
+		), ELogLevel::Debug);
+	}
+
+	FResourceNodeUtilityLog::Get().LogMessage("=== End Logging All AFGResourceNodes in the World ===", ELogLevel::Debug);
+}
+
+// UE_ENABLE_OPTIMIZATION
---
+++ Source/ResourceRoulette/Public/ResourceNodeUtility.h
@@ -0,0 +1,123 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Containers/Array.h"
+#include "Containers/Map.h"
+#include "FGResourceNode.h"
+#include "FGBuildableResourceExtractor.h"
+#include "GameInstanceModule.h"
+#include "Misc/OutputDeviceFile.h"
+#include "ResourceNodeUtility.generated.h"
+
+// DECLARE_LOG_CATEGORY_EXTERN(LogResourceRoulette, Warning, All);
+constexpr int MaxUniqueClasses = 10; // number of ore types in EOreType
+
+struct FResourceNodeConfig
+{
+	FString MeshPath;
+	FString MaterialPath;
+	FVector Offset;
+	FVector Scale;
+};
+
+UENUM(BlueprintType)
+enum class EOccupiedType : uint8
+{
+	Any,
+	Occupied,
+	Unoccupied,
+};
+
+UENUM(BlueprintType)
+enum class EOreType : uint8
+{
+	Bauxite,
+	Caterium,
+	Coal,
+	Copper,
+	Iron,
+	Limestone,
+	Quartz,
+	Sam,
+	Sulfur,
+	Uranium,
+};
+
+// Mapping from ore type names to their enum values
+const TArray<TPair<FString, EOreType>> OreTypeNameList = {
+	MakeTuple("Bauxite", EOreType::Bauxite),
+	MakeTuple("Caterium", EOreType::Caterium),
+	MakeTuple("Coal", EOreType::Coal),
+	MakeTuple("Copper", EOreType::Copper),
+	MakeTuple("Iron", EOreType::Iron),
+	MakeTuple("Stone", EOreType::Limestone),
+	MakeTuple("Quartz", EOreType::Quartz),
+	MakeTuple("Sam", EOreType::Sam),
+	MakeTuple("Sulfur", EOreType::Sulfur),
+	MakeTuple("Uranium", EOreType::Uranium)
+};
+
+// Ore types used for shuffling in the procedural generation process
+const TArray<EOreType> ShuffleTypes = {
+	EOreType::Bauxite,
+	EOreType::Caterium,
+	EOreType::Coal,
+	EOreType::Copper,
+	EOreType::Iron,
+	EOreType::Limestone,
+	EOreType::Quartz,
+	EOreType::Sulfur
+};
+
+// Ore types used for grouping resources
+const TArray<EOreType> MakeGroupTypes = {
+	EOreType::Caterium,
+	EOreType::Coal,
+	EOreType::Copper,
+	EOreType::Iron,
+	EOreType::Limestone,
+	EOreType::Quartz,
+	EOreType::Sulfur
+};
+
+enum class ELogLevel
+{
+	Debug = 2,
+	Warning = 1,
+	Error = 0
+};
+
+const FName CustomResourceNodeTag = "CustomResourceNode";
+
+class RESOURCEROULETTE_API FResourceNodeUtilityLog : public FOutputDevice
+{
+public:
+	static FResourceNodeUtilityLog& Get();
+	void InitializeLog();
+	void ShutdownLog();
+	void LogMessage(const FString& Message, ELogLevel Level = ELogLevel::Debug);
+	virtual void Serialize(const TCHAR* V, ELogVerbosity::Type Verbosity, const FName& Category) override;
+	
+private:
+	FResourceNodeUtilityLog() = default;
+	static FResourceNodeUtilityLog Instance;
+	FOutputDeviceFile* LogFile = nullptr;
+	FCriticalSection LogFileMutex;
+};
+
+UCLASS()
+class RESOURCEROULETTE_API UResourceNodeUtility: public UObject
+{
+
+GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Resource Roulette")
+	static void InitializeLoggingModule();
+	
+	static bool TryGetOreTypeFromName(const FString& Name, EOreType& OreType);
+	static bool TryGetNameFromOreType(EOreType OreType, FString& Name);
+	static bool GetOreTypeFromResourceNode(const AFGResourceNode* ResourceNode, EOreType& OutOreType);
+	static bool FindOreTypeFromPath(const FString& Path, EOreType& OutOreType);
+	static void LogAllResourceNodes(const UWorld* World);
+};
