2d2ff5d1c7050c36633e9237e2c883cd591832f3
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-08 16:29:05 +0100

    Add ResourceNodeGrouper class

---
+++ Source/ResourceRoulette/Public/ResourceNodeGrouper.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "CustomResourceNode.h"
+#include "Containers/Array.h"
+#include "Containers/Map.h"
+
+#include "ResourceNodeUtility.h"
+
+class FResourceNodeGrouper
+{
+public:
+    void AddResourceNode(const FCustomResourceNode& ResourceNode);
+    void OrganizeResourceNodeGroups(float DistanceToGroup);
+
+    const TMap<EOreType, TMap<EResourcePurity, int>>& GetOreTypeWithPurity() const;
+    const TArray<TArray<FCustomResourceNode>>& GetResourceNodeGroups() const;
+    const TMap<EOreType, TArray<FCustomResourceNode>>& GetStandardResourceNodeList() const;
+
+private:
+    void ClusterNearbyNodes(float DistanceToGroup);
+    void RemoveGroupedNodesFromStandardList();
+    bool IsNodeInAnyGroup(const FCustomResourceNode& Node) const;
+    
+    TMap<EOreType, TMap<EResourcePurity, int>> OreTypeWithPurity;
+    TArray<TArray<FCustomResourceNode>> ResourceNodeGroups;
+    TMap<EOreType, TArray<FCustomResourceNode>> StandardResourceNodeList;
+};
