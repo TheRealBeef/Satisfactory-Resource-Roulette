4e96fffce6031e66049d876ff479ccc9e90f43e2
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-08 16:30:52 +0100

    Add ResourceNodePurityManager class

---
+++ Source/ResourceRoulette/Public/ResourceNodePurityManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "FGResourceNode.h"
+#include "ResourceNodeUtility.h"
+
+#include "ResourceNodePurityManager.generated.h"
+
+class UResourceNodeSpawner;
+
+UCLASS()
+class RESOURCEROULETTE_API UResourceNodePurityManager : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UResourceNodePurityManager();
+	
+	static EResourcePurity DeterminePurityToAssign(EOreType OreType, TMap<EOreType, TMap<EResourcePurity, int>>& RemainingPurityCounts);
+	static void LogAndUpdatePurity(EOreType OreType, EResourcePurity Purity, TMap<EOreType, TMap<EResourcePurity, int>>& PurityCounts);
+
+	void CountNodePurities(const UWorld* World);
+	void UpdatePurityCount(EOreType OreType, EResourcePurity Purity);
+
+	const TMap<EOreType, TMap<EResourcePurity, int>>& GetOriginalPurityCounts() const;
+	static bool IsValidPurity(EResourcePurity Purity);
+
+private:
+	void IncrementPurityCount(EOreType OreType, EResourcePurity Purity);
+
+	TMap<EOreType, TMap<EResourcePurity, int>> OriginalPurityCounts;
+};
