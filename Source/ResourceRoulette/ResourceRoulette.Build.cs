0c0a6cb13a71a56cd7736bd06873e02a89053474
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-08 16:44:19 +0100

    Update Build.cs Disable unity build

--- Source/ResourceRoulette/ResourceRoulette.Build.cs
+++ Source/ResourceRoulette/ResourceRoulette.Build.cs
@@ -8,7 +8,7 @@ public class ResourceRoulette : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		CppStandard = CppStandardVersion.Cpp20;
-
+		bUseUnity = false;
 		// FactoryGame transitive dependencies
 		// Not all of these are required, but including the extra ones saves you from having to add them later.
 		// Some entries are commented out to avoid compile-time warnings about depending on a module that you don't explicitly depend on.
@@ -51,34 +51,10 @@ public class ResourceRoulette : ModuleRules
 			//"SignificanceISPC"
 		});
 
-		// Header stubs
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"DummyHeaders",
-		});
-
 		if (Target.Type == TargetRules.TargetType.Editor) {
 			PublicDependencyModuleNames.AddRange(new string[] {/*"OnlineBlueprintSupport",*/ "AnimGraph"});
 		}
 		PublicDependencyModuleNames.AddRange(new string[] {"FactoryGame", "SML"});
-		
-		PublicIncludePaths.AddRange(new string[] {
-			// ... add public include paths required here ...
-		});
-		
-		PrivateIncludePaths.AddRange(new string[] {
-			// ... add private include paths required here ...
-		});
-		
-		PublicDependencyModuleNames.AddRange(new string[] {
-			// ... add public dependencies that you statically link with here ...
-		});
-		
-		PrivateDependencyModuleNames.AddRange(new string[] {
-			// ... add private dependencies that you statically link with here ...	
-		});
-		
-		DynamicallyLoadedModuleNames.AddRange(new string[] {
-			// ... add any modules that your module loads dynamically here ...
-		});
+
 	}
 }
