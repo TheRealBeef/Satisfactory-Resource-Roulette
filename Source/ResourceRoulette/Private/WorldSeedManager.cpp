28d9076a0c1a6b8aa599df1e52ec41048deb1767
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-08 16:43:57 +0100

    Add WorldSeedManager class

---
+++ Source/ResourceRoulette/Private/WorldSeedManager.cpp
@@ -0,0 +1,65 @@
+﻿#include "WorldSeedManager.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/World.h"
+#include "Engine/Engine.h"
+#include "ResourceNodeUtility.h"
+
+void AWorldSeedManager::SetGlobalSeed(int32 const NewSeed) {
+    Seed = NewSeed;
+}
+
+int32 AWorldSeedManager::GetGlobalSeed() const {
+    return Seed;
+}
+
+AWorldSeedManager::AWorldSeedManager()
+{
+    PrimaryActorTick.bCanEverTick = false;
+    bReplicates = true;
+}
+
+void AWorldSeedManager::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AWorldSeedManager::OnWorldInitialized(const UWorld::FActorsInitializedParams&)
+{
+    const int32 CurrentSeed = GetGlobalSeed();
+    FResourceNodeUtilityLog::Get().LogMessage(FString::Printf(TEXT("Loaded Seed: %d"), CurrentSeed), ELogLevel::Debug);
+}
+
+void AWorldSeedManager::PostLoadGame_Implementation(int32 SaveVersion, int32 GameVersion)
+{
+    FWorldDelegates::OnWorldInitializedActors.AddUObject(this, &AWorldSeedManager::OnWorldInitialized);
+}
+
+int32 AWorldSeedManager::GenerateSeed()
+{
+    return FMath::Rand() * FMath::Rand();
+}
+
+void AWorldSeedManager::InitRandom()
+{
+    if (Seed == 0)
+    {
+        SetGlobalSeed(GenerateSeed());
+        FResourceNodeUtilityLog::Get().LogMessage(FString::Printf(TEXT("Generated Seed: %d"), Seed), ELogLevel::Debug);
+    }
+}
+
+bool AWorldSeedManager::ShouldSave_Implementation() const
+{
+    return true;
+}
+
+void AWorldSeedManager::PreSaveGame_Implementation(int32 SaveVersion, int32 GameVersion)
+{
+}
+
+void AWorldSeedManager::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AWorldSeedManager, Seed);
+}
---
+++ Source/ResourceRoulette/Public/WorldSeedManager.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "FGSaveInterface.h"
+#include "WorldSeedManager.generated.h"
+
+UCLASS()
+class RESOURCEROULETTE_API AWorldSeedManager : public AActor, public IFGSaveInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	AWorldSeedManager();
+
+	virtual void BeginPlay() override;
+
+	int32 GetGlobalSeed() const;
+	void SetGlobalSeed(int32 NewSeed);
+	void OnWorldInitialized(const UWorld::FActorsInitializedParams& ActorsInitializedParams);
+
+	static int32 GenerateSeed();
+	void InitRandom();
+
+	virtual bool ShouldSave_Implementation() const override;
+	virtual void PreSaveGame_Implementation(int32 SaveVersion, int32 GameVersion) override;
+	virtual void PostLoadGame_Implementation(int32 SaveVersion, int32 GameVersion) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+private:
+	UPROPERTY(Replicated)
+	int32 Seed = 0;
+};
