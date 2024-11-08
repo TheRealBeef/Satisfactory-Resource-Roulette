4d88695fa19c9518b07da1b5f6eda15cb4d39c88
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-08 16:43:22 +0100

    Add ResourceRouletteSubsystem class

---
+++ Source/ResourceRoulette/Private/ResourceRouletteSubsystem.cpp
@@ -0,0 +1,69 @@
+﻿#include "ResourceRouletteSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+#include "WorldSeedManager.h"
+#include "EngineUtils.h"
+
+AResourceRouletteSubsystem* AResourceRouletteSubsystem::Get(const UObject* WorldContext)
+{
+    if (!WorldContext) return nullptr;
+    return Cast<AResourceRouletteSubsystem>(UGameplayStatics::GetActorOfClass(WorldContext, AResourceRouletteSubsystem::StaticClass()));
+}
+
+void AResourceRouletteSubsystem::BeginPlay()
+{
+    Super::BeginPlay();
+    InitializeResourceRoulette();
+}
+
+void AResourceRouletteSubsystem::InitializeResourceRoulette()
+{
+    if (!GetWorld()) return;
+    ResourceNodeSpawner = NewObject<UResourceNodeSpawner>(this);
+    SetupWorldSeedManager(GetWorld());
+    bResourcesInitialized = true;
+    constexpr float UpdateInterval = 1.5f;
+    GetWorld()->GetTimerManager().SetTimer(UpdateTimerHandle, this, &AResourceRouletteSubsystem::UpdateResourceNodes, UpdateInterval, true);
+    FResourceNodeUtilityLog::Get().LogMessage("Resource Roulette initialized successfully.", ELogLevel::Debug);
+}
+
+void AResourceRouletteSubsystem::SetupWorldSeedManager(UWorld* World)
+{
+    for (TActorIterator<AWorldSeedManager> It(World); It; ++It)
+    {
+        SeedManager = *It;
+        break;
+    }
+    if (!SeedManager.IsValid())
+    {
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+        SeedManager = World->SpawnActor<AWorldSeedManager>(AWorldSeedManager::StaticClass(), FTransform(FVector()), SpawnParams);
+        if (SeedManager.IsValid())
+        {
+            SeedManager->InitRandom();
+        }
+    }
+}
+
+void AResourceRouletteSubsystem::UpdateResourceNodes()
+{
+    if (!GEngine || !GetWorld() || !ResourceNodeSpawner || !SeedManager.IsValid())
+    {
+        FResourceNodeUtilityLog::Get().LogMessage("UpdateResourceNodes aborted: Missing dependencies.", ELogLevel::Error);
+        return;
+    }
+    int32 GlobalSeed = SeedManager->GetGlobalSeed();
+    ResourceNodeSpawner->SetSeed(GlobalSeed);
+    ResourceNodeSpawner->ScanWorldResourceNodes(GetWorld());
+    ResourceNodeSpawner->ReplaceStandardResourceNodesUpdate(GetWorld());
+
+    FResourceNodeUtilityLog::Get().LogMessage("Resource nodes updated successfully.", ELogLevel::Debug);
+}
+
+void AResourceRouletteSubsystem::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    GetWorld()->GetTimerManager().ClearTimer(UpdateTimerHandle);
+    Super::EndPlay(EndPlayReason);
+}
---
+++ Source/ResourceRoulette/Public/ResourceRouletteSubsystem.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystem/ModSubsystem.h"
+#include "ResourceNodeSpawner.h"
+#include "WorldSeedManager.h"
+#include "ResourceRouletteSubsystem.generated.h"
+
+UCLASS()
+class RESOURCEROULETTE_API AResourceRouletteSubsystem : public AModSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	static AResourceRouletteSubsystem* Get(const UObject* WorldContext);
+
+	UFUNCTION(BlueprintCallable)
+	void InitializeResourceRoulette();
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateResourceNodes();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool AreResourcesInitialized() const { return bResourcesInitialized; }
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+	void SetupWorldSeedManager(UWorld* World);
+
+private:
+	bool bResourcesInitialized = false;
+
+	TWeakObjectPtr<AWorldSeedManager> SeedManager;
+	UPROPERTY()
+	UResourceNodeSpawner* ResourceNodeSpawner;
+	FTimerHandle UpdateTimerHandle;
+};
